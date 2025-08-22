7a2f23aab7e76dd641142691159d7ae8463db565
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-08-22 10:40:53 +0200

    feat: Creating baseUi component and Home page, passing baseUi component to app.html with default style for all pages

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { SharedModule } from './modules/shared-module';
+import { Home } from './pages/home/home';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
+import { BaseUi } from './components/base-ui/base-ui';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Home,
+    Navbar,
+    Footer,
+    BaseUi
   ],
   imports: [
     BrowserModule,
