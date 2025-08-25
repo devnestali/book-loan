69c7c96aee59e146f956af7574a88e7d903fa4a9
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-08-25 10:57:57 +0200

    feat: Creating clients form page with bootstrap component styles

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Clients } from './pages/clients/clients';
+import { ClientForms } from './pages/client-forms/client-forms';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'client',
     component: Clients
+  },
+  {
+    path: 'client/create',
+    component: ClientForms
   }
 ];
 
