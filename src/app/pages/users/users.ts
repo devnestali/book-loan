fc7c0cface1f79568656f31b3af3b204e420cdb4
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-09-11 15:02:33 +0200

    feat: Creating function selectUsers and pageChanged to show items on pagination, using ngx-bootstrap pagination

--- src/app/pages/users/users.ts
+++ src/app/pages/users/users.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { Router } from '@angular/router';
+import { Pagination } from '../../models/pagination';
+import { UserService } from '../../services/user';
 
 @Component({
   selector: 'app-users',
@@ -8,40 +10,42 @@ import { Router } from '@angular/router';
   templateUrl: './users.html',
   styleUrl: './users.css'
 })
-export class Users {
-  constructor(private router: Router) {}
+export class Users implements OnInit {
+  users: User[] = []
+  pagination: Pagination | undefined
+  pageNumber = 1
+  pageSize = 5
 
-  users: User[] =
-  [
-    {
-      id: 0,
-      nome: 'John Doe',
-      email: 'johndoe@example.com',
-      password: undefined,
-      isAdmin: true,
-      ativo: true
-    },
-    {
-      id: 1,
-      nome: 'Josh Doe',
-      email: 'johndoe1@example.com',
-      password: undefined,
-      isAdmin: false,
-      ativo: true
-    },
-    {
-      id: 1,
-      nome: 'Jack Doe',
-      email: 'johndoe2@example.com',
-      password: undefined,
-      isAdmin: false,
-      ativo: true
-    },
-  ]
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) {}
 
+  ngOnInit(): void {
+    this.selectUsers()
+  }
+
+  selectUsers() {
+    this.userService.selectUsers(this.pageNumber, this.pageSize).subscribe({
+      next: (response) => {
+        if(response.result && response.pagination) {
+          this.users = response.result
+          this.pagination = response.pagination
+        }
+      },
+    })
+  }
+
+  pageChanged(event: any) {
+    if(this.pageNumber !== event.page) {
+      this.pageNumber = event.page
+      this.selectUsers()
+    }
+  }
 
 
   changeOrDeleteUser(user: User) {
     this.router.navigate(['users/put'], { state: { user }})
   }
 }
+
