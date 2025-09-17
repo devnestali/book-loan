5d31cd6bb1c9c90a9e6eff415dd47d9f5975cf14
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-09-17 12:01:57 +0200

    feat: Creating reactive form on bookForms page to add books via includeBook function and initializeForm function to validate inputs

--- src/app/pages/book-forms/book-forms.ts
+++ src/app/pages/book-forms/book-forms.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../../models/book';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BookService } from '../../services/book';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-book-forms',
@@ -6,6 +11,49 @@ import { Component } from '@angular/core';
   templateUrl: './book-forms.html',
   styleUrl: './book-forms.css'
 })
-export class BookForms {
+export class BookForms implements OnInit{
+  book?: Book
+  bookForms: FormGroup = new FormGroup({})
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private bookService: BookService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    const currentNavigate = this.router.currentNavigation()
+    if(currentNavigate?.extras.state) {
+      this.book = currentNavigate.extras.state['book']
+    }
+  }
+
+  ngOnInit(): void {
+    this.initializeForm()
+    if(this.book) {
+      this.bookForms.setValue(this.book)
+    }
+  }
+
+  initializeForm() {
+    this.bookForms = this.formBuilder.group({
+      livroNome: ['', [Validators.required, Validators.maxLength(50)]],
+      livroAutor: ['', [Validators.required, Validators.maxLength(200)]],
+      livroEditora: ['', [Validators.required, Validators.maxLength(50)]],
+      livroAnoPublicacao: ['', [Validators.required]],
+      livroEdicao: ['', [Validators.required, Validators.maxLength(50)]]
+    })
+  }
+
+  includeBook() {
+    if(this.bookForms.valid) {
+      this.bookService.includeBook(this.bookForms.value).subscribe({
+        next: (response) => {
+          console.log(response)
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      })
+    }
+  }
 }
