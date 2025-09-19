79f7553249312a13fbbbb400a330c50eccac6ea3
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-09-19 11:40:31 +0200

    feat: Crating includeClient and changeClient function, making some updates on initializeForm and injects some services

--- src/app/pages/client-forms/client-forms.ts
+++ src/app/pages/client-forms/client-forms.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Client } from '../../models/client';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ClientService } from '../../services/client-service';
+import { translateMessages } from '../../utils/translateMessages';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-client-forms',
@@ -13,7 +16,12 @@ export class ClientForms implements OnInit {
   client?: Client
   clientForms: FormGroup = new FormGroup({})
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private clientService: ClientService,
+    private toastr: ToastrService
+  ) {
     const currentNavigation = this.router.currentNavigation()
 
     if(currentNavigation?.extras.state) {
@@ -32,16 +40,46 @@ export class ClientForms implements OnInit {
   initializeForm() {
     this.clientForms = this.formBuilder.group(
       {
-        Id: [0, [Validators.required, Validators.minLength(0)]],
-        CliCPF: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11),]],
-        CliNome: ['', [Validators.required, Validators.maxLength(200)]],
-        CliEndereco: ['', [Validators.required, Validators.maxLength(50)]],
-        CliCidade: ['', [Validators.required, Validators.maxLength(50)]],
-        CliBairro: ['', [Validators.required, Validators.maxLength(50)]],
-        CliNumero: ['', [Validators.required, Validators.maxLength(50)]],
-        CliTelefoneCelular: ['', [Validators.required, Validators.maxLength(11)]],
-        CliTelefoneFixo: ['', [Validators.required, Validators.maxLength(10)]],
+        id: [0, [Validators.required, Validators.minLength(0)]],
+        cliCPF: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11),]],
+        cliNome: ['', [Validators.required, Validators.maxLength(200)]],
+        cliEndereco: ['', [Validators.required, Validators.maxLength(50)]],
+        cliCidade: ['', [Validators.required, Validators.maxLength(50)]],
+        cliBairro: ['', [Validators.required, Validators.maxLength(50)]],
+        cliNumero: ['', [Validators.required, Validators.maxLength(50)]],
+        cliTelefoneCelular: ['', [Validators.required, Validators.maxLength(11)]],
+        cliTelefoneFixo: ['', [Validators.required, Validators.maxLength(10)]],
       }
     )
+  }
+
+  includeClient() {
+    if(this.clientForms.valid) {
+      this.clientService.includeClient(this.clientForms.value).subscribe({
+        next: (response) => {
+          const translatedSuccessResponse = translateMessages(response.message)
+          this.toastr.success(translatedSuccessResponse)
+        },
+        error: (response) => {
+          const translatedErrorResponse = translateMessages(response.error)
+          this.toastr.error(translatedErrorResponse)
+        }
+      })
+    }
+  }
+
+  changeClient() {
+    if(this.clientForms.valid) {
+      this.clientService.changeClient(this.clientForms.value).subscribe({
+        next: (response) => {
+          const translatedSuccessResponse = translateMessages(response.message)
+          this.toastr.success(translatedSuccessResponse)
+        },
+        error: (response) => {
+          const translatedErrorResponse = translateMessages(response.error)
+          this.toastr.error(translatedErrorResponse)
+        }
+      })
+    }
   }
 }
